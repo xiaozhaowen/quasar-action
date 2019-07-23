3bea1e8fdc581a05b361aba81f0b45c76e843f3c
xiaozhao <ab055089wuxiaozhao@ab-insurance.com>
2019-07-23 17:52:06 +0800

    init

--- src/main/java/com/xiaozhao/office/HelloWorld.java
+++ src/main/java/com/xiaozhao/office/HelloWorld.java
@@ -2,6 +2,9 @@ package com.xiaozhao.office;
 
 import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.strands.SuspendableCallable;
+import co.paralleluniverse.strands.SuspendableRunnable;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutionException;
 
@@ -9,12 +12,21 @@ import java.util.concurrent.ExecutionException;
  * Fiber 这个类除了构造函数，start，join，get之外不应该再访问这个类的其他方法。
  * Fiber在模拟线程，如果需要操作类似线程的一些行为，请使用Strand 这个类，这个类是对线程和协程的抽象
  *
+ * 有3种方式：
+ * 1）Fiber中覆盖run方法
+ * 2）Fiber构造函数中传递 SuspendableCallable
+ * 3）Fiber构造函数中传递 SuspendableRunnable
+ *
  * @author xiaozhao
  * @date 2019-07-2315:03
  */
+@Slf4j
 public class HelloWorld {
     public static void main(String[] args) {
+        fiberStart3();
+    }
 
+    private static void fiberStart1() {
         /**
          * 启动一个协程
          */
@@ -41,4 +53,28 @@ public class HelloWorld {
         System.out.println(result);
         System.out.println("Hello Quasar");
     }
+
+    private static void fiberStart2() {
+        new Fiber(new SuspendableCallable() {
+            @Override
+            public Object run() throws SuspendExecution, InterruptedException {
+                log.info("SuspendableCallable Fiber");
+                return "Hello";
+            }
+        }).start();
+
+        log.info("外部执行");
+    }
+
+    private static void fiberStart3() {
+        new Fiber(new SuspendableRunnable() {
+            @Override
+            public void run() throws SuspendExecution, InterruptedException {
+                log.info("SuspendableRunnable Fiber");
+            }
+        }).start();
+
+        log.info("外部执行");
+    }
 }
+
---
+++ src/main/java/com/xiaozhao/office/SuspendMethod.java
@@ -0,0 +1,64 @@
+package com.xiaozhao.office;
+
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.strands.Strand;
+import co.paralleluniverse.strands.SuspendableCallable;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * 挂起方法：凡是声明抛出SuspendExecution异常的方法，被称为挂起方法
+ * 挂起方法需要抛出SuspendExecution 异常
+ * <p>
+ * 如果一个方法A是挂起方法，如果方法B要调用方法A，那么方法B也需要声明为挂起方法
+ * <p>
+ * 挂起方法有2种形式：
+ * 1）抛出 SuspendExecution异常，这种是推荐的形式，因为编译器防止你忘记抛出 SuspendExecution
+ * 2）使用@Suspendbale注解。这种形式适用于方法已经抛出了其他异常或者继承了接口，接口方法本身有异常抛出。所以需要使用注解的形式
+ *
+ * @author xiaozhao
+ * @date 2019-07-2317:17
+ */
+@Slf4j
+public class SuspendMethod {
+    public static void main(String[] args) {
+        log.info("主线程开始1");
+
+        Fiber<String> fiber = new Fiber<String>(new SuspendableCallable<String>() {
+            @Override
+            public String run() throws SuspendExecution, InterruptedException {
+                SuspendMethod suspendMethod = new SuspendMethod();
+                String addr = suspendMethod.getAddress();
+                return addr;
+            }
+        }).start();
+
+        log.info("主线程开始2");
+
+        try {
+            String addr = fiber.get();
+            log.info("获取到值：{}", addr);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        log.info("主线程结束");
+
+    }
+
+
+    private String getAddress() throws SuspendExecution, InterruptedException {
+        String name = getName(24);
+        Strand.sleep(2000);
+        return name + "  is in NewYork";
+    }
+
+    private String getName(int id) throws SuspendExecution, InterruptedException {
+        Strand.sleep(3000);
+        return "Kobe Bryant " + id;
+    }
+}
